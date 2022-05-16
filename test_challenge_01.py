a66b54aa4ffa2e29265693116748546093183995
artkontraer <t@artkontraer.com>
2022-05-16 21:06:14 +0200

    solved

--- test_challenge_01.py
+++ test_challenge_01.py
@@ -4,7 +4,18 @@
 # should be counted as vowels — not y.
 
 def count_vowels(word):
-    pass
+    count = 0
+    for each_character in word:
+        if each_character == 'a' or each_character == 'e' or each_character == 'i' or each_character == 'o' or each_character == 'u':
+            count = count + 1 
+    return count
 
+  
 def test_challenge_01_happy_case(): 
      assert count_vowels('Kaleidoscope') == 6   
+     assert count_vowels('Theresa') == 3
+
+def test_challenge_01_emptycase():
+    assert count_vowels ('') == 0
+    
+# find other test possibilities, run the tests 
