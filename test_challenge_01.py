41f2aa06b8f6d086d05d4b833bff3211a56d6d73
artkontraer <t@artkontraer.com>
2022-05-17 16:09:59 +0200

    tests added

--- test_challenge_01.py
+++ test_challenge_01.py
@@ -6,7 +6,7 @@
 def count_vowels(word):
     count = 0
     for each_character in word:
-        if each_character == 'a' or each_character == 'e' or each_character == 'i' or each_character == 'o' or each_character == 'u':
+        if each_character == 'a' or each_character == 'e' or each_character == 'i' or each_character == 'o' or each_character == 'u' or each_character == 'A' or each_character == 'E' or each_character == 'I' or each_character == 'O' or each_character == 'U':
             count = count + 1 
     return count
 
@@ -17,5 +17,11 @@ def test_challenge_01_happy_case():
 
 def test_challenge_01_emptycase():
     assert count_vowels ('') == 0
-    
-# find other test possibilities, run the tests 
+    assert count_vowels("brrr") == 0
+
+def test_challenge_01_Upper_case():
+    assert count_vowels('VOLUME') == 3
+    #failed, therefore made changes to if-conditions and added UPPERCASE vowels
+
+def test_challenge_01_mixed_types():
+    assert count_vowels('Chanel No. 5') == 3
