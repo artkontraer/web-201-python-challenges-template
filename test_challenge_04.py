2a52392d950591ac82f74010c1510b4c9bbf7659
artkontraer <t@artkontraer.com>
2022-05-23 19:46:26 +0200

    solved

--- test_challenge_04.py
+++ test_challenge_04.py
@@ -6,13 +6,11 @@
 # True or False if the variable is a palindrome.
 
 def is_palindrome(var):
-    var = str()
+    var = str(var) 
     if var == var[::-1]:
         return True 
-            
-
-
-
+    else:
+        return False
 
 def test_challenge_04_palindrome_number():
     assert is_palindrome(545) == True
@@ -22,3 +20,7 @@ def test_challenge_04_palindrome_string():
 
 def test_challenge_04_palindrome_mix():
     assert is_palindrome('moX2Xom') == True    
+
+def test_challenge_04_palindrome_fail():
+    assert is_palindrome('moxx') == False    
+
