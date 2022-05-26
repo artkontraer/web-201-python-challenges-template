3946daac7e4597506f1e6dbe430a57dc23887543
artkontraer <t@artkontraer.com>
2022-05-26 18:11:43 +0200

    solved

--- test_challenge_07.py
+++ test_challenge_07.py
@@ -6,8 +6,21 @@
 # if total is greater than 70 Euros 7% discount and so on.
 
 def calculate_bill(shopping_list):
-    pass
+    sumItems = sum(shopping_list.values())
+    discount = (sumItems // 10) * sumItems / 100
+    if sumItems > 50:
+        sumItems -= discount
+    return(sumItems)
+    # else:
+    #     return(sumItems)
+
+# shopping_list = {'apples':11.20, 'bananas':2.2, 'eggs':30.00}
+
 
 def test_challenge_07_happy_case(): 
     shopping_list = {'apples':11.20, 'bananas':2.2, 'eggs':30.00}
     assert calculate_bill(shopping_list) == 43.4
+
+def test_challenge_07_happy_case_2(): 
+    shopping_list = {'apples':15, 'bananas':20, 'eggs':16}
+    assert calculate_bill(shopping_list) == 48.45
